f22fa85119b099de625f3c0906a9ea66f6e9e6d5
ivan <ivanzigonipires.11@gmail.com>
2023-10-14 16:01:31 -0300

    initial commit and globo-paywall script

---
+++ globo-paywall.js
@@ -0,0 +1,30 @@
+;(() => {
+  /**
+   * removes paywall from https://oglobo.globo.com/ articles
+   * usage:
+   *  - go to article's page
+   *  - open dev tools (ctrl+shift+i)
+   *  - go to console tab
+   *  - copy this script 
+   *  - paste it over the console
+   *  - hit enter
+   *  - exit the dev tools (ctrl+shift+i)
+   */
+  const selectors = {
+    modal: 'div[id="container-id"]',
+    greyWall: 'div[class="barreiraJornada"]',
+    html: 'html',
+    footer: 'div[class="mobiliarioFooter"]',
+  };
+  for (const entry of Object.entries(selectors)) {
+    const [element, selector] = entry;
+    const domElement = document.querySelector(selector);
+    if (domElement) {
+      if (element === "html") {
+        domElement.setAttribute("style", "touch-action: none;");
+      } else {
+        domElement.remove();
+      } 
+    }
+  }
+})()
